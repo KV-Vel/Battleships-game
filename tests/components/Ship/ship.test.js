0aa31a29c1f9700cc19548bbe60398d9e1b77508
SprngVel <kraevvalentin98@gmail.com>
2025-02-16 12:18:13 +1000

    moved tests folder

---
+++ tests/components/Ship/ship.test.js
@@ -0,0 +1,39 @@
+import Ship from '../../../src/components/Ship/ship';
+
+let ship;
+beforeEach(() => {
+    ship = new Ship(3);
+});
+
+test('class methods have been called', () => {
+    const spyOnHit = jest.spyOn(ship, 'hit');
+    const spyOnisSunk = jest.spyOn(ship, 'isSunk');
+
+    ship.hit();
+    ship.isSunk();
+
+    expect(spyOnHit).toHaveBeenCalled();
+    expect(spyOnisSunk).toHaveBeenCalled();
+});
+
+test('hitTimes variable updates', () => {
+    const spyOnGetter = jest.spyOn(ship, 'hitTimes', 'get');
+
+    ship.hit();
+    ship.hit();
+    ship.hitTimes;
+
+    expect(spyOnGetter).toHaveBeenCalled();
+    expect(ship.hitTimes).toEqual(2);
+});
+
+test('isSunk returns correct value according to the times ship is been hit', () => {
+    ship.hit();
+    ship.hit();
+
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+
+    expect(ship.isSunk()).toBe(true);
+});
