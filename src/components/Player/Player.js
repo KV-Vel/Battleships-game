d922cb923b4acda5874c045e2c3cf82bd978a8de
SprngVel <kraevvalentin98@gmail.com>
2025-03-01 10:17:03 +1000

    feature: init player class Tests will be added later

---
+++ src/components/Player/Player.js
@@ -0,0 +1,10 @@
+export default class Player {
+    shipsQuantity;
+
+    constructor(type, gameboard) {
+        this.type = type;
+        this.gameboard = gameboard;
+    }
+}
+
+// Что то делать с Import export ошибкой?
