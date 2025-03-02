1c2ba9e16e3c00147336a50894600ac56bba5186
SprngVel <kraevvalentin98@gmail.com>
2025-03-02 10:54:08 +1000

    init battleship test file

---
+++ tests/components/Battleship/battleship.test.js
@@ -0,0 +1,8 @@
+import Player from "../../../src/components/Player/Player";
+import Battleship from "../../../src/components/Battleship/Battleship";
+
+const game = new Battleship(new Player('human', {}), new Player('human', {}));
+
+test.skip('checking if game can be started should return players statuses', () => {
+
+});
