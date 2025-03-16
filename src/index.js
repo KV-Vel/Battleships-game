d2884904943bda60459625f1e6794b256482e5b3
SprngVel <kraevvalentin98@gmail.com>
2025-03-16 13:01:02 +1000

    refactor: modified how game created

--- src/index.js
+++ src/index.js
@@ -2,46 +2,32 @@ import "./styles/reset-css.css";
 import "./styles/styles.css";
 import ShipRandomizer from "./components/Randomizer/ShipRandomizer";
 import Player from "./components/Player/Player";
-import Gameboard from "./components/Gameboard/gameboard";
+import Gameboard from "./components/Gameboard/Gameboard";
 import Battleship from "./components/Battleship/Battleship";
 import BoardUI from "./components/UI/BoardUI";
+import EventListenerManager from "./components/Controller/EventListenerManager";
+
+import pubsub from "./utils/PubSub";
 
 const ROWS = 10;
 const COLS = 10;
 
 const randomizer = new ShipRandomizer(ROWS, COLS);
 
+const gameboard1 = new Gameboard(ROWS, COLS);
+const gameboard2 = new Gameboard(ROWS, COLS);
 
-const gameboard1 = new Gameboard(ROWS, COLS, randomizer);
-const gameboard2 = new Gameboard(ROWS, COLS, randomizer);
+const player1 = new Player("p1", gameboard1);
+const player2 = new Player("p2", gameboard2);
 
-const player1 = new Player("human", gameboard1);
-const player2 = new Player("human", gameboard2);
+const game = new Battleship(player1, player2, randomizer);
 
 const domPlayField = document.querySelector(".play-field");
-const game = new Battleship(player1, player2, new BoardUI(domPlayField));
+const ui = new BoardUI(domPlayField);
 
-// player1.gameboard.placeShipRandomly([
-//     new Ship(4),
-//     new Ship(3),
-//     new Ship(3),
-//     new Ship(2),
-//     new Ship(2),
-//     new Ship(2),
-//     new Ship(1),
-//     new Ship(1),
-//     new Ship(1),
-//     new Ship(1),
-// ]);
+pubsub.subscribe("randomlyAdd", ui.displayShip.bind(ui));
+pubsub.subscribe("attack", ui.displayAttackResult.bind(ui));
 
-console.log(player1.gameboard);
-
-// game.canStartGame();
-// tEST
-// const entries = Object.entries(game.player1.shipsQuantity);
-// for (let i = 4; i >= 1; i--) {
-//     game.player1.gameboard.placeShipRandomly(new Ship(i));
-//     console.log(game.player1);
-// }
-
-// eslint to everyfile
+ui.createBoard(ROWS, COLS, player1.name);
+ui.createBoard(ROWS, COLS, player2.name);
+const eventListenerManager = new EventListenerManager(game, ui);
