cbfce1dab9898fa17b430c681c3b8769d7d52314
SprngVel <kraevvalentin98@gmail.com>
2025-03-25 12:20:17 +1000

    another modules initiation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./styles/reset-css.css";
 import "./styles/styles.css";
+import Settings from "./components/Battleship/Settings";
 import ShipRandomizer from "./components/Randomizer/ShipRandomizer";
 import Player from "./components/Player/Player";
+import AI from "./components/Player/AiPlayer";
 import Gameboard from "./components/Gameboard/Gameboard";
-import Battleship from "./components/Battleship/Battleship";
+import Battleship from "./components/Battleship/ActivePhase";
 import BoardUI from "./components/UI/BoardUI";
 import EventListenerManager from "./components/Controller/EventListenerManager";
 
@@ -12,22 +14,38 @@ import pubsub from "./utils/PubSub";
 const ROWS = 10;
 const COLS = 10;
 
-const randomizer = new ShipRandomizer(ROWS, COLS);
+const settings = new Settings([
+    ["4", 1],
+    ["3", 2],
+    ["2", 3],
+    ["1", 4],
+]);
 
 const gameboard1 = new Gameboard(ROWS, COLS);
 const gameboard2 = new Gameboard(ROWS, COLS);
 
-const player1 = new Player("p1", gameboard1);
-const player2 = new Player("p2", gameboard2);
+const randomizer = new ShipRandomizer(10, 10);
 
-const game = new Battleship(player1, player2, randomizer);
+const player1 = new Player("p1", gameboard1, randomizer, settings.shipsQuantity);
+const ai = new Player("ai", gameboard2, randomizer, settings.shipsQuantity);
+
+const prepphase = new Battleship(player1, ai);
 
 const domPlayField = document.querySelector(".play-field");
 const ui = new BoardUI(domPlayField);
 
 pubsub.subscribe("randomlyAdd", ui.displayShip.bind(ui));
 pubsub.subscribe("attack", ui.displayAttackResult.bind(ui));
+pubsub.subscribe("isReady", ui.toggleReadyBtn.bind(ui));
 
 ui.createBoard(ROWS, COLS, player1.name);
-ui.createBoard(ROWS, COLS, player2.name);
-const eventListenerManager = new EventListenerManager(game, ui);
+ui.createBoard(ROWS, COLS, ai.name);
+const eventListenerManager = new EventListenerManager(prepphase, ui);
+
+// @todo block random ship if game started
+// @todo unused function in gameboard (clearBoard());
+// @todo remove addShipsRandomly() in AI constructor?
+// @todo add delay before ai makes move;
+// @todo surround placed ships
+// @todo make randomBtn to recreate random placement repetitively and resetBoard during this
+// @todo prepphase not needed?? потому что смысла от нее 0, и только bothPlayersReady нужна
