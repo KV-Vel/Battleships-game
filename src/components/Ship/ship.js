46458d4da62ec3e7d40a516b86eed500e273e368
SprngVel <kraevvalentin98@gmail.com>
2025-04-08 10:11:00 +1000

    fix: conscturctor wasnt creating unique id for ship, fixed

--- src/components/Ship/ship.js
+++ src/components/Ship/ship.js
@@ -1,11 +1,15 @@
 export default class Ship {
+    static shipsCreated = 0;
+
     #hitTimes = 0;
 
     #sunk = false;
 
     constructor(length) {
         this.length = Number(length);
-        this.identificator = new Date().getTime().toString();
+        this.identificator = Ship.shipsCreated.toString();
+
+        Ship.shipsCreated += 1;
     }
 
     get hitTimes() {
